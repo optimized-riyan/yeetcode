a1f79777eccee83c108403e8c5d24e084cea5c7e
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-10 12:11:07 +0530

    trying to get vue-inertia to work, gonna try breeze

---
+++ resources/js/Pages/User/Layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <main>
+    <header>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
+    </header>
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
