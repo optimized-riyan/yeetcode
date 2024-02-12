898b2f8515256c35884adf71e4269532fcc80f2b
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-12 09:42:29 +0530

    inited factories and inited ProblemSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ProblemSeeder::class,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
