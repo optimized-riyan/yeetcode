97d4b191a8a280e76077aaff9a7b0d8389cfee84
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-12 15:37:52 +0530

    got the seeding to work again, reason: im dumb

--- database/migrations/2024_02_12_015652_create_problems_table.php
+++ database/migrations/2024_02_12_015652_create_problems_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('problems', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('difficulty')->constrained()->cascadeOnDelete();
         });
     }
 
