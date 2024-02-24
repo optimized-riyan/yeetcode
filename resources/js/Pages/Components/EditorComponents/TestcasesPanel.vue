f904520690033e4755f637e606cdd97d9a151be8
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-24 23:25:49 +0530

    added reactive console panels

---
+++ resources/js/Pages/Components/EditorComponents/TestcasesPanel.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <ul>
+        <li v-for="(tcParam, index) in tcParameters" :key="index">
+            <TestcaseParam :parameterName="tcParameters[index]" :parameterContent="testcaseArray[currentTestcase].testcase"></TestcaseParam>
+        </li>
+    </ul>
+</template>
+<script>
+import TestcaseParam from './TestcaseParam.vue';
+export default {
+    props: {
+        tcParameters: Array
+    },
+    components: {
+        TestcaseParam,
+    }
+}
+</script>
+<style lang="">
+
+</style>
--- resources/js/Pages/EditorPage.vue
+++ resources/js/Pages/EditorPage.vue
@@ -20,19 +20,51 @@ defineProps({ problem: Object, trivialTestcases: Object });
                 </div>
                 <!-- console -->
                 <div class="grow">
-                    <!-- testcases -->
-                    <ul class="flex">
-                        <li v-for="(testcase, index) in testcaseArray" :key="index">
-                            <button type="button" @click="testcaseChange(index)">Testcase {{index+1}}</button>
-                        </li>
-                    </ul>
-                    <!-- testcase content -->
+                    <!-- panel change buttons -->
                     <div>
-                        <ul>
-                            <li v-for="(tcParam, index) in tcParameters" :key="index">
-                                <TestcaseParam :parameterName="tcParameters[index]" :parameterContent="testcaseArray[currentTestcase].testcase"></TestcaseParam>
-                            </li>
-                        </ul>
+                        <button type="button" @click="()=>this.consolePanel='testcases'">Testcases</button>
+                        <button type="button" @click="()=>this.consolePanel='results'">Test Results</button>
+                    </div>
+                    <!-- panel contents -->
+                    <div>
+                        <div v-if="consolePanel == 'testcases'">
+                            <!-- testcases -->
+                            <ul class="flex">
+                                <li v-for="(testcase, index) in testcaseArray" :key="index">
+                                    <button type="button" @click="testcaseChange(index)">Testcase {{index+1}}</button>
+                                </li>
+                            </ul>
+                            <!-- testcase content -->
+                            <div>
+                                <ul>
+                                    <li v-for="(tcParam, index) in tcParameters" :key="index">
+                                        <TestcaseParam :parameterName="tcParameters[index]" :parameterContent="testcaseArray[currentTestcase].testcase"></TestcaseParam>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <!-- testcases -->
+                            <ul class="flex">
+                                <li v-for="(testcase, index) in testcaseArray" :key="index">
+                                    <button type="button" @click="testcaseChange(index)">Testcase {{index+1}}</button>
+                                </li>
+                            </ul>
+                            <!-- testcase content -->
+                            <div>
+                                <ul>
+                                    <li v-for="(tcParam, index) in tcParameters" :key="index">
+                                        <TestcaseParam :parameterName="tcParameters[index]" :parameterContent="testcaseArray[currentTestcase].testcase"></TestcaseParam>
+                                    </li>
+                                    <li>
+                                        <TestcaseParam parameterName="Your output" :parameterContent="testcaseOutputs[currentTestcase]"></TestcaseParam>
+                                    </li>
+                                    <li>
+                                        <TestcaseParam parameterName="Expected output" :parameterContent="expectedOutputs[currentTestcase]"></TestcaseParam>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -48,6 +80,9 @@ export default {
             currentTestcase: 0,
             testcaseArray: Object.values(this.problem.testcases),
             tcParameters: this.problem.tc_parameters.params.split(' '),
+            consolePanel: 'testcases',
+            testcaseOutputs: ['gay1', 'gay2', 'gay3', 'gay4'],
+            expectedOutputs: ['straight1', 'straight2', 'straight3', 'straight4']
         }
     },
     components: {
