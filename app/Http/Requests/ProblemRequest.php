d46e59464291188f178882c5d7f189d07132def6
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-27 19:18:04 +0530

    added buttons in form

--- app/Http/Requests/ProblemRequest.php
+++ app/Http/Requests/ProblemRequest.php
@@ -22,7 +22,7 @@ class ProblemRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|max:127'
+            'name' => 'required|max:127',
         ];
     }
 }
--- resources/js/Pages/StoreProblem.vue
+++ resources/js/Pages/StoreProblem.vue
@@ -12,10 +12,23 @@
         </div>
         <!-- examples -->
         <div>
+            <ul v-for="(example, index) in form.examples" :key="index">
+                <li>
+                    {{ example }}
+                </li>
+            </ul>
+            <span>Add Example: </span><textarea ref="examplesTextarea" name="examples" cols="30" rows="10"></textarea>
+            <button type="button" @click="()=>{form.examples.push(this.$refs.examplesTextarea.value)}">Add</button>
         </div>
-        <button type="button">Add Example</button>
         <!-- testcases -->
         <div>
+            <ul v-for="(testcase, index) in form.testcases" :key="index">
+                <li>
+                    {{ testcase }}
+                </li>
+            </ul>
+            <span>Add Testcase: </span><textarea ref="testcasesTextarea" name="testcases" cols="30" rows="10"></textarea>
+            <button type="button" @click="()=>{form.examples.push(this.$refs.testcasesTextarea.value)}">Add</button>
         </div>
         <button type="button">Add Testcase</button>
         <!-- topics -->
@@ -27,12 +40,24 @@
         <button type="button">Add Similar Problem</button>
         <!-- hints -->
         <div>
+            <ul v-for="(hint, index) in form.hints" :key="index">
+                <li>
+                    {{ hint }}
+                </li>
+            </ul>
+            <span>Add Hint: </span><textarea ref="hintsTextarea" name="hints" cols="30" rows="10"></textarea>
+            <button type="button" @click="()=>{form.examples.push(this.$refs.hintsTextarea.value)}">Add</button>
         </div>
-        <button type="button">Add Hint</button>
         <!-- constraints -->
         <div>
+            <ul v-for="(testcase, index) in form.testcases" :key="index">
+                <li>
+                    {{ testcase }}
+                </li>
+            </ul>
+            <span>Add Constraint: </span><textarea ref="constraintsTextarea" name="constraints" cols="30" rows="10"></textarea>
+            <button type="button" @click="()=>{form.examples.push(this.$refs.constraintsTextarea.value)}">Add</button>
         </div>
-        <button type="button">Add Constraint</button>
     </form>
 </template>
 <script>
