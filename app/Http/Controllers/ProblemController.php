c7c0eb35a8247cbead5c2722113a969cff278be7
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-28 07:04:01 +0530

    added endpoint for new problem submission, gotta refactor er diagram for normalization

--- app/Http/Controllers/ProblemController.php
+++ app/Http/Controllers/ProblemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProblemRequest;
+use App\Models\Example;
 use Illuminate\Http\Request;
 use App\Models\Problem;
 use Inertia\Inertia;
@@ -46,7 +47,19 @@ class ProblemController extends Controller
     {
         $form = $request->validated();
 
-        // $problem = Problem;
+        $problem = new Problem;
+        $problem->name = $form->name;
+        $problem->description = $form->description;
+
+        foreach ($form->examples as $example) {
+            $exampleModel = new Example;
+            $exampleModel->input = $example->input;
+            $exampleModel->output = $example->output;
+            $exampleModel->explaination = $example->explaination;
+            $problem->examples()->save($exampleModel);
+        }
+        foreach ($form->constraints as $constraint) {
+        }
 
         return to_route('problems.index');
     }
