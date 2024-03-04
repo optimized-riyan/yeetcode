c30f63665fb051b563dcc415d552a88988df418d
Riyan Gonsalves <riyan0350@gmail.com>
2024-03-04 18:41:43 +0530

    custom log channel created

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'debug' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/debug.log'),
+            'level' => 'debug',
+        ],
     ],
 
 ];
