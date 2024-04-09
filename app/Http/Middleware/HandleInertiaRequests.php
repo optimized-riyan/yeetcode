c50099485c9257d7a23e7699e0b3f78f87bf41e7
Riyan Gonsalves <riyan0350@gmail.com>
2024-04-10 02:34:35 +0530

    add onlineCompilerDomain export

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'onlineCompilerDomain' => env('ONLINE_COMPILER_DOMAIN'),
         ];
     }
 }
