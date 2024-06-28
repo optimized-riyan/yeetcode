fc7fe9c6b17cfffceedca5bf03358e762d9c5d8f
Riyan Gonsalves <riyan0350@gmail.com>
2024-06-29 05:18:14 +0530

    fix colors being dropped instead of being extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,37 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
-        },
-        colors: {
-            leetcode: {
-                background: '#1A1A1A',
-                backgroundlight: '#282828',
-                backgroundlighter: '#3E3E3E',
-                textdark: '#3D3D3E',
-                text: '#C2C4C7',
-                textlight: '#FFFFFF',
-                green: '#28934D',
-                "green-light": "#38CE6D",
-                purple: '#8748A7',
-                blue: '#124F8C',
-                red: '#F5365C',
-                yellow: '#FFC01E',
-                navbar: '#282828',
-            }
+            colors: {
+                leetcode: {
+                    background: "#1A1A1A",
+                    backgroundlight: "#282828",
+                    backgroundlighter: "#3E3E3E",
+                    textdark: "#3D3D3E",
+                    text: "#C2C4C7",
+                    textlight: "#FFFFFF",
+                    green: "#28934D",
+                    "green-light": "#38CE6D",
+                    purple: "#8748A7",
+                    blue: "#124F8C",
+                    red: "#F5365C",
+                    yellow: "#FFC01E",
+                    navbar: "#282828",
+                },
+            },
         },
     },
 
