aa36548fd1bbc9f5122be23333030f07419ea42c
Riyan Gonsalves <riyan0350@gmail.com>
2024-06-29 05:53:02 +0530

    trying out propped colors

---
+++ resources/js/Pages/Components/EditorComponents/SolidButton.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <button type="button" :class="`${bgColor} ${textColor} ${bgColorHover} ${textColorHover}`">
+        <slot />
+    </button>
+</template>
+<script>
+export default {
+    props: {
+        bgColor: {
+            type: String,
+            default: "bg-leetcode-backgroundlighter",
+        },
+        textColor: {
+            type: String,
+            default: "text-leetcode-green",
+        },
+        bgColorHover: {
+            type: String,
+            default: "hover:bg-leetcode-backgroundlight",
+        },
+        textColorHover: {
+            type: String,
+            default: "hover:text-black",
+        }
+    }
+}
+</script>
+ <!-- hover:${textColorHover} -->
