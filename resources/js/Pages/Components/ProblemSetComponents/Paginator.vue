cc9aec97d26ab766f7597e5316bc3c437ae4c0d7
Riyan Gonsalves <riyan0350@gmail.com>
2024-07-02 06:55:48 +0530

    - make paginator look better

--- resources/js/Pages/Components/ProblemSetComponents/Paginator.vue
+++ resources/js/Pages/Components/ProblemSetComponents/Paginator.vue
@@ -1,12 +1,12 @@
 <template>
     <div v-if="links.length > 3">
-        <div class="flex flex-wrap -mb-1">
+        <div class="flex flex-wrap">
             <template v-for="(link, key) in links">
                 <div v-if="link.url === null" :key="key"
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
+                    class="mr-1 px-4 py-2 text-sm leading-4 text-leetcode-text rounded flex items-center" v-html="link.label" />
                 <Link v-else
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500"
-                    :class="{ 'bg-white': link.active }" :href="link.url" v-html="link.label" />
+                    class="mr-1 px-3 py-2 text-sm leading-4 rounded hover:bg-leetcode-backgroundlighter flex items-center"
+                    :class="{ 'bg-leetcode-backgroundlight': link.active }" :href="link.url" v-html="link.label" />
             </template>
         </div>
     </div>
