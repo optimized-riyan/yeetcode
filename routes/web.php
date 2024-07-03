0f3c522bc97de20a0944d617e08d516948c73405
Riyan Gonsalves <riyan0350@gmail.com>
2024-07-03 09:17:33 +0530

    - remove some stuff from authenticated layout - set TextInput text color to leetcode-text - remove dashboard page from web.php

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,6 @@ Route::middleware('auth')->get('/contactme', function() {
     ]);
 })->name('contactme');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
