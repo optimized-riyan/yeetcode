8b18744cac192ba0acee29d7b213761ffddcca93
Riyan Gonsalves <riyan0350@gmail.com>
2024-07-03 08:15:34 +0530

    - change input label and input textbox colors on login page

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">
+    <label class="block font-medium text-sm text-gray-700 dark:text-leetcode-text">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
