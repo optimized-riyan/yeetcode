ee449dade8f21a6d3d6ab2d647dc572de9e5388c
Riyan Gonsalves <riyan0350@gmail.com>
2024-07-03 08:42:17 +0530

    - change final color themes on login page - register page as well as a bonus

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -60,19 +60,19 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                    <span class="ms-2 text-sm text-gray-600 dark:text-leetcode-text">Remember me</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <Link :href="route('register')"
-                class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100
+                class="underline text-sm text-gray-600 dark:text-leetcode-text hover:text-gray-900 dark:hover:text-gray-100
                 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 me-4"
                 >
                     New Here?
                 </Link>
                 <Link v-if="canResetPassword" :href="route('password.request')"
-                class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md
+                class="underline text-sm text-gray-600 dark:text-leetcode-text hover:text-gray-900 dark:hover:text-gray-100 rounded-md
                 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 >
                     Forgot your password?
