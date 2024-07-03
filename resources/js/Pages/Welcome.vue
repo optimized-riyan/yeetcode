987fb684fb29582ecdfbdb27098b9a3aea599d2d
Riyan Gonsalves <riyan0350@gmail.com>
2024-07-03 07:15:49 +0530

    - add more style to welcome page, need to fix logout and display logout button when user is logged in

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,21 +1,27 @@
 <template lang="">
     <div class="h-screen w-screen bg-leetcode-background flex flex-col justify-center items-center text-leetcode-text">
-        <div class="h-2/6 w-4/12 bg-leetcode-backgroundlight flex flex-col px-8 py-6 justify-between rounded-xl">
+        <div class="h-1/4 w-4/12 bg-leetcode-backgroundlight flex flex-col px-8 py-6 justify-between rounded-xl">
             <div class="flex flex-col items-center">
                 <div class="self-center font-bold text-4xl">Welcome to YeetCode!</div>
-                <div class="mt-12">An online coding platform made with <span class="font-semibold">emoji barrage</span> using Laravel, Vue and MySQL and more technologies by yours truly, Riyan Gonsalves!</div>
+                <div class="mt-4">
+                    An online coding platform made with
+                    <span>
+                        <i class="fa-solid fa-heart text-leetcode-red"></i>
+                    </span>
+                    using Laravel, Vue and MySQL and more technologies by yours truly, Riyan Gonsalves!
+                </div>
             </div>
             <div class="flex justify-between">
                 <div class="flex gap-8">
-                    <Link>
+                    <Link :href="route('login')">
                         <u>Login</u>
                     </Link>
-                    <Link>
+                    <Link :href="route('register')">
                         <u>Register</u>
                     </Link>
                 </div>
                 <div>
-                    <Link>
+                    <Link :href="route('problems.index')">
                         <u>Go to problems page</u>
                     </Link>
                 </div>
@@ -27,8 +33,27 @@
 import { Link } from "@inertiajs/vue3";
 
 export default {
+    data() {
+        return {
+            emojiIndex: 0,
+            emojis: [
+                `<i class="fa-solid fa-heart"></i>`,
+                `<i class="fa-regular fa-face-laugh-squint"></i>`,
+                `<i class="fa-solid fa-face-grin-tears"></i>`,
+                `<i class="fa-regular fa-face-sad-cry"></i>`,
+            ],
+        }
+    },
     components: {
         Link,
-    }
+    },
+    methods: {
+        nextEmoji() {
+            this.emojiIndex = (this.emojiIndex + 1) % this.emojis.length;
+        }
+    },
+    mounted() {
+        console.log(this.$page.props);
+    },
 }
 </script>
