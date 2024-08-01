55afb7d4194ca71111fbb5efd65d71157f686151
Riyan Gonsalves <riyan0350@gmail.com>
2024-08-02 02:05:21 +0530

    chore: add favicon link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="/favicon-32x32.png">
 
     <!-- Scripts -->
     @routes
